69fe0a1c1e346e0b812b689d6a04b114fb7ff85f
bhanuprakash026 <bhanutankasala26@gmail.com>
2023-11-23 11:33:51 +0530

    configured tailwind css

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
+        <h1 className="text-8xl">
             Contact page
-        </div>
+        </h1>
     )
 }
 
